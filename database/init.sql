ab183bc27782486a42157fc45d03cab6924e0e7c
Gizoskos <gizemgultoprak2004@gmail.com>
2025-03-20 14:36:36 +0300

    metrics system yaml

--- database/init.sql
+++ database/init.sql
@@ -1,26 +1,23 @@
--- Booking-Service ve Payment-Service için veritabanını kullan
-\c postgres;
--- Eğer veritabanı varsa sil
--- Eğer veritabanı zaten varsa, işlemi atla
-DO $$ BEGIN IF EXISTS (
+-- Create database if it doesn't exist
+DO $$ BEGIN IF NOT EXISTS (
   SELECT
   FROM pg_database
   WHERE datname = 'booking_db'
-) THEN RAISE NOTICE 'Database booking_db already exists';
-ELSE CREATE DATABASE booking_db;
+) THEN PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE booking_db');
 END IF;
-END $$ -- Veritabanına bağlan
-\c booking_db;
--- Payments tablosunu oluştur
-CREATE TABLE payments (
+END $$;
+-- Connect to the 'booking_db' database (Note: you can't use \c in scripts)
+\ c booking_db;
+-- Create payments table
+CREATE TABLE IF NOT EXISTS payments (
   id SERIAL PRIMARY KEY,
   user_id INT NOT NULL,
   amount DECIMAL(10, 2) NOT NULL,
   createdAt TIMESTAMP DEFAULT NOW(),
   updatedAt TIMESTAMP DEFAULT NOW()
 );
--- Bookings tablosunu oluştur (Foreign Key ile)
-CREATE TABLE bookings (
+-- Create bookings table with foreign key reference
+CREATE TABLE IF NOT EXISTS bookings (
   id SERIAL PRIMARY KEY,
   user_id INT NOT NULL,
   payment_id INT NOT NULL,
